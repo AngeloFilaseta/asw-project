f196308f226416d3cd2746a5da7f0df33f720262
tale97 <alessandro.talmi@studio.unibo.it>
2021-04-17 15:23:34 +0200

    aggiunto token allo store

--- client/src/redux/userInfo/actions.js
+++ client/src/redux/userInfo/actions.js
@@ -3,11 +3,19 @@ export const setUsername = (username) => {
         type: "SET_USERNAME",
         payload: username
     };
-};
+}
 
 export const setId = (id) => {
     return {
         type: "SET_ID",
         payload: id
     };
-};
+}
+
+export const setToken = (token) => {
+    return {
+        type: "SET_TOKEN",
+        payload: token
+    };
+}
+
--- client/src/redux/userInfo/reducer.js
+++ client/src/redux/userInfo/reducer.js
@@ -1,12 +1,14 @@
 var initialState = {
     username: null,
-    id: null
+    id: null,
+    token: null
 }
 
 const userInfoReducer = (state = initialState, action) => {
     switch(action.type){
         case 'SET_USERNAME': state.username = action.payload; return state;
         case 'SET_ID': state.id = action.payload; return state;
+        case 'SET_TOKEN': state.token = action.payload; return state;
         default: return state;
     }
 }
