8ed0146cbb0ea2fcc47093d46307ce5618e0659d
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-05-04 12:53:21 +0200

    Added welcome notification

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -5,12 +5,14 @@ const User = require("../mongoose/model/user");
 const bcrypt = require("bcryptjs");
 const jsonwebtoken = require("jsonwebtoken");
 const key = require("../conf/conf");
+const NotificationFactory = require("../mongoose/model/factory/notification");
 
 async function signup(req, res) {
     let newUser = UserFactory.createUser(req.body.username, req.body.password);
     await User.findOne({ username: newUser["username"] }).then(async profile => {
         if (!profile) {
             newUser.save().then(() => {
+                createWelcomeNotification(newUser.id)
                 Responses.OKResponse(res, newUser);
             }).catch(err => {
                 Errors.ServerError(res, {message: err.message});
@@ -30,7 +32,6 @@ async function login(req, res) {
         if (!profile) {
             Errors.NotFoundError(res, {message: "Wrong credentials inserted."});
         } else {
-            console.log(newUser["password"] + " " + profile.password);
             bcrypt.compare(
                 newUser["password"],
                 profile.password,
@@ -66,6 +67,12 @@ async function login(req, res) {
 
 function getProfile(req, res) {
     Responses.OKResponse(res, {id: req.user.id, username: req.user.username});
+}
+
+
+function createWelcomeNotification(userId){
+    let newNotification = NotificationFactory.createNotification("Welcome to GuessR!", "Have fun playing :)", new Date(), userId);
+    newNotification.save().then(() => { /* does nothing */ });
 }
 
 module.exports = {
