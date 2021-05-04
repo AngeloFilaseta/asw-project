8391fa3f372844b24df2e4d409ea66468421ee59
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-05-04 12:40:34 +0200

    Solved crash on report show phase

---
+++ client/src/react/lobby/showReports/ReportEntry.js
@@ -0,0 +1,49 @@
+import {Button, Collapse} from "react-bootstrap";
+import SVG from "react-inlinesvg";
+import {useState} from "react";
+
+export default function ReportEntry(props){
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+        <Button block size="lg" variant="info"
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}>
+            Show {props.username}'s report
+        </Button>
+        <Collapse in={open}>
+            <div className="border border-info rounded" align="center">
+                {alternateListsElements(props.sentences, props.draws)
+                    .map((entry, index) => renderContent(entry, index))}
+            </div>
+        </Collapse>
+        </>
+    )
+
+    function alternateListsElements(sentence, draw) {
+        let run = 0, first = 0, second = 0;
+        const newArr = [];
+        while (run < sentence.length + draw.length) {
+            if (first > second) {
+                newArr[run] = draw[second];
+                second++;
+            } else {
+                newArr[run] = sentence[first];
+                first++;
+            }
+            run++;
+        }
+        return newArr;
+    }
+
+    function renderContent(entry, index){
+        if(index % 2 === 0){
+            return <h2 key={"entry " + index}>"{entry}"</h2>
+        } else {
+            return <SVG src={entry} key={"entry " + index}/>
+        }
+    }
+}
--- client/src/react/lobby/showReports/ShowReports.js
+++ client/src/react/lobby/showReports/ShowReports.js
@@ -1,11 +1,8 @@
 import { useSelector, useDispatch } from "react-redux"
-
-import SVG from "react-inlinesvg"
-import { Button, Collapse } from "react-bootstrap"
-import $ from "jquery"
-
+import { Button } from "react-bootstrap"
 import PlayerTypes from "../../../util/playerType"
 import {sendEndReport} from "../LobbyLogic"
+import ReportEntry from "./ReportEntry";
 
 export default function ShowReports(){
     
@@ -16,70 +13,24 @@ export default function ShowReports(){
     let reports = useSelector(state => state.lobby.reports)
     let isAdmin = useSelector(state => state.lobby.info.isMyRoleAdmin)
 
-    if (isAdmin === PlayerTypes.ADMIN) {
-        return (
-            <div className="">
-                <div className="mx-3">
-                    <Button block className="mt-3" size="lg" onClick={() => sendEndReport(dispatch, socket, id_user, lobbyCode)}>Back to Lobby</Button>
-                </div>
-                {reportShow(reports)}
-            </div>
-        )
-    } else {
-        return (
+    return (
             <div className="mx-3">
+                {isAdmin === PlayerTypes.ADMIN &&
+                    <Button block className="mt-3" size="lg"
+                            onClick={() => sendEndReport(dispatch, socket, id_user, lobbyCode)}>
+                        Back to Lobby
+                    </Button>
+                }
                 {reportShow(reports)}
-            </div>
-        )
-    }
+            </div>)
 }
 
 function reportShow(reports) {
-
-    let i = 0;
-    let j = 0;
-
-    return (
-        <>
-            {reports.map((report, index) => (
-                <div key={"report " + i++}>
-                    <br />
-                    <Button block size="lg" variant="info" onClick={() => handleCollapse(index)}
-                        aria-controls={"collapse-div-" + index}
-                        aria-expanded={false}
-                    >
-                        Show {report.username}'s report
-                    </Button>
-                    <Collapse>
-                        <div id={"collapse-div-" + index} className="border border-info rounded " align="center">
-                            {alternateListsElements(report.sentences, report.draws).map((entry, index) => (
-                                index % 2 === 0 ? <h2 key={"entry " + j++}>"{entry}"</h2> : <SVG src={entry} key={"entry " + j++}/>
-                            ))}
-                        </div>
-                    </Collapse>
+    return (reports.map((report, index) => (
+                <div className={"py-1"} key={"report " + index}>
+                    <ReportEntry username={report.username}
+                                 sentences={report.sentences}
+                                 draws={report.draws}/>
                 </div>
-            ))}
-        </>
-    );
-}
-
-function handleCollapse(index) {
-    $('#collapse-div-' + index).collapse('toggle');
-}
-
-
-function alternateListsElements(sentence, draw) {
-    let run = 0, first = 0, second = 0;
-    const newArr = [];
-    while (run < sentence.length + draw.length) {
-        if (first > second) {
-            newArr[run] = draw[second];
-            second++;
-        } else {
-            newArr[run] = sentence[first];
-            first++;
-        }
-        run++;
-    }
-    return newArr;
+            )));
 }
