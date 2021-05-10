477f7d7e8e22e3be7ff1e61c31619e2a54e0e366
Piero Sanchi <pierosanchi97@gmail.com>
2021-05-10 23:08:47 +0200

    updated pdf card style

--- client/src/react/showPreviousReports/ReportCard.js
+++ client/src/react/showPreviousReports/ReportCard.js
@@ -1,19 +1,28 @@
 import { Card, Button } from "react-bootstrap"
 import { Col } from "react-bootstrap"
 
+import { AwesomeButton } from "react-awesome-button";
+
 export default function ReportCard(props) {
 
     return (
         <>
             <Col className="col-lg-3 col-12 col-md-6 mt-3">
-                <Card>
+                <Card className="border-dark" style={{ background: "rgba(255,255,255, 0.4)" }}>
                     <Card.Header>
                         <Card.Title align="center">
                             {props.title}
                         </Card.Title>
                     </Card.Header>
                     <Card.Body>
-                        <Button block variant="primary" onClick={() => props.handler()}>Download PDF</Button>
+                        
+                        <AwesomeButton onPress={() => props.handler()}
+                            className="mb-2"
+                            style={{ display: 'block', width: '100%', fontSize: 18 }}
+                            type="primary" ripple="true">
+                            Download PDF
+                        </AwesomeButton>
+
                     </Card.Body>
                 </Card>
             </Col>
