b594ef1756bb909d5ef59cacad6c16ac85e64181
PieroSanchi <piero.sanchi@studio.unibo.it>
2021-05-10 14:34:48 +0200

    added bell icon and updated background

--- client/src/react/showPreviousReports/ShowPreviousReports.js
+++ client/src/react/showPreviousReports/ShowPreviousReports.js
@@ -3,6 +3,14 @@ import { useSelector } from "react-redux"
 import { RedirectAuthentication } from "../common/GuessrRedirect"
 import ReportPage from "./ReportPage"
 
+import back from "../../img/background.svg"
+
 export default function ShowPreviousReports(){
-    return useSelector(state => state.userInfo.username) !== null ? <ReportPage /> : <RedirectAuthentication/>
+    return useSelector(state => state.userInfo.username) !== null ? <div style={{backgroundImage:`url(${back})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    width: '100vw',
+    height: '100vh' }}>
+        <ReportPage />
+    </div>   : <RedirectAuthentication/>
 }
