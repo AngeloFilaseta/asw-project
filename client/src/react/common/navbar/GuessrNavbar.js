959298b903ca9d0deabd3a6b2212cc6fe7ffdc53
Piero Sanchi <pierosanchi97@gmail.com>
2021-05-13 17:28:07 +0200

    added sliders, removed button from login

--- client/src/react/common/navbar/GuessrNavbar.js
+++ client/src/react/common/navbar/GuessrNavbar.js
@@ -1,34 +1,16 @@
 import React from "react"
-import { Button, Col } from "react-bootstrap"
-import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+import { Col } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 
+import { useSelector } from "react-redux"
 import GuessrLogout from "./GuessrLogout"
 import GuessrNotification from "./GuessrNotification"
 import GuessrBrand from "./GuessrBrand";
 
+
 export default function GuessrNavbar(props) {
 
     return (
-        /*
-        <Navbar bg="dark" variant="dark" id="navbar">
-            <Col className="col-4 col-sm-3">
-                <GuessrBrand/>
-            </Col>
-            <Col className="col-4 col-sm-6">
-                <div className="text-white  text-center py-3" >
-                    <h3 className="d-none d-sm-inline" title="title">{props.title}</h3>
-                    <h6 className="d-sm-none d-inline">{props.title}</h6>
-                    <h4 title="subtitle">{props.subtitle}</h4>
-                </div>
-            </Col>
-            <Col className="col-4 col-sm-3 d-flex justify-content-end">
-                <Nav>
-                   <GuessrNotification/>
-                   <GuessrLogout/>
-                </Nav>
-            </Col>
-        </Navbar>*/
-
         <>
             <div className="d-none d-md-inline">
                 <Navbar bg="dark" variant="dark" id="navbar">
@@ -66,17 +48,17 @@ export default function GuessrNavbar(props) {
                         </div>
                     </Nav>
 
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse className="dropdown-menu-end" id="responsive-navbar-nav">
-                        <Nav className="ml-auto" >
-                            <GuessrNotification/>
-                            <GuessrLogout />
-                        </Nav>
-
-                    </Navbar.Collapse>
+                    {useSelector(state => state.userInfo.username) === null ? (<></>) : (<>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse className="dropdown-menu-end" id="responsive-navbar-nav">
+                            <Nav className="ml-auto" >
+                                <GuessrNotification />
+                                <GuessrLogout />
+                            </Nav>
+                        </Navbar.Collapse>
+                    </>)}
 
                 </Navbar>
-
             </div>
         </>
     )
--- client/src/react/lobby/draw/Draw.js
+++ client/src/react/lobby/draw/Draw.js
@@ -8,57 +8,41 @@ import ClearButton from "./ClearButton"
 import EraserButton from "./EraserButton"
 import SubmitDrawButton from "./SubmitDrawButton"
 import GameTimer from "../../common/GameTimer"
-import {submitDraw} from "../LobbyLogic"
-import {DRAW_MAX_TIME} from "../../../util/global"
+import { submitDraw } from "../LobbyLogic"
+import { DRAW_MAX_TIME } from "../../../util/global"
 
-import { AwesomeButton } from "react-awesome-button"
 import { useState } from "react"
+import Slider from "rc-slider"
+import "rc-slider/assets/index.css"
 
-export default function Draw(props){
+export default function Draw(props) {
 
-    const [renderRef, draw] = useSvgDrawing({penWidth: drawPenSize})
+    const [renderRef, draw] = useSvgDrawing({ penWidth: drawPenSize })
     const [penWidth, setPenWidth] = useState(drawPenSize)
 
     let timeExpireHandler = () => submitDraw(props.dispatch, props.socket, props.id_user, props.report_to_id, props.lobbyCode, draw.getSvgXML())
-    return(
+    return (
         <LoadingOverlay active={props.waitingAllSubmit} spinner text='Waiting for other users to submit their drawings...'>
             <ReceivedSentence />
             <Canvas renderRef={renderRef} />
             <div align="center">
                 {!props.waitingAllSubmit &&
-                <>
-                    <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                        <EraserButton draw={draw}/>
-                        <GameTimer timeExpireHandler={timeExpireHandler} nSeconds={DRAW_MAX_TIME} />
-                        <ClearButton draw={draw}/>
-                    </div>
-                    <ColorPicker draw={draw} penWidth={penWidth}/>
-                    <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                        <AwesomeButton 
-                            onPress={() => {setPenWidth(penWidth - 1); draw.changePenWidth(penWidth);}} 
-                            className="mb-2" 
-                            style={{ display: 'inline', fontSize: 22}} 
-                            type="primary" 
-                            ripple={true}
-                            size="small"
-                            disabled={penWidth === 1 ? true : false}
-                        >-</AwesomeButton>
-                        <h3 style={{ display: 'inline', marginLeft:"3px", marginRight:"3px" }}> Width: {penWidth} </h3>
-                        <AwesomeButton 
-                            onPress={() => {setPenWidth(penWidth + 1); draw.changePenWidth(penWidth);}} 
-                            className="mb-2" 
-                            style={{ display: 'inline', fontSize: 22}} 
-                            type="primary" 
-                            ripple={true}
-                            size="small"
-                            disabled={penWidth === 50 ? true : false}
-                        >+</AwesomeButton>
-                    </div>
-                    <SubmitDrawButton draw={draw}/>
-                </>
+                    <>
+                        <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                            <EraserButton draw={draw} />
+                            <GameTimer timeExpireHandler={timeExpireHandler} nSeconds={DRAW_MAX_TIME} />
+                            <ClearButton draw={draw} />
+                        </div>
+                        <ColorPicker draw={draw} penWidth={penWidth} />
+                        <Slider min={1} max={50} defaultValue={drawPenSize} onChange={v => { draw.changePenWidth(v); setPenWidth(v); }} />
+                        <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                            <h3 style={{ display: 'inline', marginLeft: "3px", marginRight: "3px" }}> Stroke width: {penWidth} </h3>
+                        </div>
+                        <SubmitDrawButton draw={draw} />
+                    </>
                 }
             </div>
         </LoadingOverlay>
     )
-    
+
 }
