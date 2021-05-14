5102d47a48b14fae27d67cade4a8ce16787a4d84
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-05-14 10:23:24 +0200

    Fixed bug of wrong number of turns in some cases

--- client/src/react/lobbyCreation/form/TurnsInput.js
+++ client/src/react/lobbyCreation/form/TurnsInput.js
@@ -10,6 +10,11 @@ export default function TurnsInput() {
     const dispatch = useDispatch()
     const [localNTurns, setLocalNTurns] = useState(DEFAULT_N_TURNS)
 
+    const addTurnHandler = () => { let newTurn = localNTurns + 1;
+                                   setNewTurn(dispatch, newTurn);}
+    const removeTurnHandler = () => { let newTurn = localNTurns - 1;
+                                      setNewTurn(dispatch, newTurn);}
+
     return (
         <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <Button
@@ -17,8 +22,8 @@ export default function TurnsInput() {
                 type="primary"
                 size="small"
                 style={{ display: 'inline', fontSize: 22, minWidth: "5vw", marginRight: 10 }}
-                disabled={localNTurns === MIN_N_TURNS ? true : false}
-                onClick={() => { setLocalNTurns(localNTurns - 1); dispatch(setNTurns(localNTurns)); }}>
+                disabled={localNTurns === MIN_N_TURNS}
+                onClick={removeTurnHandler}>
                 -
             </Button>
             <h3 style={{ display: 'inline', marginLeft: "3px", marginRight: "3px" }}> Turns: {localNTurns} </h3>
@@ -27,11 +32,16 @@ export default function TurnsInput() {
                 type="primary"
                 size="small"
                 style={{ display: 'inline', fontSize: 22, minWidth: "5vw", marginLeft: 10 }}
-                disabled={localNTurns === MAX_N_TURNS ? true : false}
-                onClick={() => { setLocalNTurns(localNTurns + 1); dispatch(setNTurns(localNTurns)); }}>
+                disabled={localNTurns === MAX_N_TURNS}
+                onClick={addTurnHandler}>
                 +
             </Button>
         </div>
     )
+
+    function setNewTurn(dispatch, newTurn){
+        setLocalNTurns(newTurn);
+        dispatch(setNTurns(newTurn));
+    }
 
 }
